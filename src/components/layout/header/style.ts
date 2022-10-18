587f4f07abb548a56589a79fa3a1668cf0b72919
kiarash98e <kiarashsafaei0@gmail.com>
2022-10-18 07:25:12 +0330

    styling

---
+++ src/components/layout/header/style.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+
+export const Header = styled.header`
+    height: 5rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0 auto;
+    background-color: #778D45;
+    width: 100%;
+    
+`
+
+export const Btn = styled.button`
+    margin: 0 .75rem;
+    padding: .75rem;
+    color: #F7d818;
+    font-size: 1rem;
+    font-weight: bold;
+    background-color: transparent;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    text-transform: uppercase;
+`
